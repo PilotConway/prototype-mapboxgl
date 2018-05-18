2feab4c95918de6cfc16863f35a8b530c7f5f9b9
John Conway <john.conway@mcmurdo.orolia.com>
2018-05-18 11:52:38 -0400

    Removed non useful code and added test Map comp

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,9 @@ import pkg from './package.json';
 export default [
 	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: 'src/index.js',
 		output: {
-			name: 'howLongUntilLunch',
+			name: 'prototype-mapboxgl',
 			file: pkg.browser,
 			format: 'umd'
 		},
@@ -21,10 +21,10 @@ export default [
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
 	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
+	// an array for the `output` option, where we can specify
 	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
+		input: 'src/index.js',
 		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
